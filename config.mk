fb9e63e77f98058f45dc85184e85cac25cc31288
Prabir <mail@prabir.me>
2019-11-29 13:26:46 -0800

    enable compiling against mingw

--- config.mk
+++ config.mk
@@ -1,12 +1,17 @@
 # dwm-win32 version
 VERSION = alpha2
 
-CFLAGS = -target x86_64-windows-msvc -std=c99 -pedantic -Wall -Os -DVERSION=\"${VERSION}\"
+CPPFLAGS = -DVERSION=\"${VERSION}\"
+
+# clang
+CFLAGS = -target x86_64-windows-msvc -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
 LDFLAGS = -target x86_64-windows-msvc
+CC = clang
+
+# mingw64
+# CFLAGS = -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
+# LDFLAGS = -s -mwindows -ldwmapi
 
 ifeq ($(OS),Windows_NT)
 	EXE=.exe
 endif
-
-# compiler and linker
-CC = clang
