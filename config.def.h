ca9e5ee34639a89fbc59cfce10a81629de881757
Prabir <prshrest@microsoft.com>
2019-12-08 18:30:18 -0800

    fix togglebar shortcut key

--- config.def.h
+++ config.def.h
@@ -67,7 +67,7 @@ static const char *termcmd[]  = { "cmd.exe", NULL };
 
 static Key keys[] = {
     /* modifier                     key        function             argument */
-    /* { MODKEY|MOD_SHIFT,             VK_RETURN, spawn,               {.v = termcmd } }, */
+    { MODKEY|MOD_SHIFT,             VK_RETURN, spawn,               {.v = termcmd } },
     { MODKEY,                       'B',       togglebar,           {0} },
     { MODKEY,                       'J',       focusstack,          {.i = +1 } },
     { MODKEY,                       'K',       focusstack,          {.i = -1 } },
