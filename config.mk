59f527b51c68b3bb76b835b895480b33e957b48e
Nir Lichtman <nir_lichtman@hotmail.com>
2020-03-02 20:37:07 +0200

    Fixed bug in which Qt5 applications were floating instead of being tiled, improved makefile

--- config.mk
+++ config.mk
@@ -3,14 +3,25 @@ VERSION = alpha2
 
 CPPFLAGS = -DVERSION=\"${VERSION}\"
 
+TARGET = x86_64-windows-msvc 
+
+ifeq ($(32BIT),1)
+	TARGET = i386-windows-msvc 
+endif
+
 # clang
-CFLAGS = -target x86_64-windows-msvc -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
-LDFLAGS = -target x86_64-windows-msvc
+CFLAGS = -target ${TARGET} -std=c99 -pedantic -Wall ${CPPFLAGS}
+LDFLAGS = -target ${TARGET}
 CC = clang
 
-# mingw64
-# CFLAGS = -std=c99 -pedantic -Wall -Os ${CPPFLAGS}
-# LDFLAGS = -s -mwindows -ldwmapi
+ifeq ($(DEBUG),1)
+	CFLAGS += -g -gcodeview -O0
+	LDFLAGS += -g
+else
+	CFLAGS += -Os
+endif
+
+
 
 ifeq ($(OS),Windows_NT)
 	EXE=.exe
