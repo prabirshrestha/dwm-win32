bccf65777689480d394598d3dc50220bac2343dd
Pascal Senn <senn.pasc@gmail.com>
2020-05-19 20:39:09 +0200

    feat: add move stack

--- dwm-win32.c
+++ dwm-win32.c
@@ -144,6 +144,7 @@ void drawborder(Client *c, COLORREF color);
 void eprint(const wchar_t *errstr, ...);
 static void focus(Client *c);
 static void focusstack(const Arg *arg);
+static void movestack(const Arg *arg);
 static Client *getclient(HWND hwnd);
 LPWSTR getclientclassname(HWND hwnd);
 LPWSTR getclienttitle(HWND hwnd);
@@ -1472,6 +1473,56 @@ zoom(const Arg *arg) {
     focus(c);
     arrange();
 }
+
+void
+movestack(const Arg *arg) {
+	Client *c = NULL, *p = NULL, *pc = NULL, *i;
+
+	if(arg->i > 0) {
+		/* find the client after selmon->sel */
+		for(c = sel->next; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
+		if(!c)
+			for(c = clients; c && (!ISVISIBLE(c) || c->isfloating); c = c->next);
+
+	}
+	else {
+		/* find the client before sel */
+		for(i = clients; i != sel; i = i->next)
+			if(ISVISIBLE(i) && !i->isfloating)
+				c = i;
+		if(!c)
+			for(; i; i = i->next)
+				if(ISVISIBLE(i) && !i->isfloating)
+					c = i;
+	}
+	/* find the client before sel and c */
+	for(i = clients; i && (!p || !pc); i = i->next) {
+		if(i->next == sel)
+			p = i;
+		if(i->next == c)
+			pc = i;
+	}
+
+	/* swap c and sel clients in the clients list */
+	if(c && c != sel) {
+		Client *temp = sel->next==c?sel:sel->next;
+		sel->next = c->next==sel?c:c->next;
+		c->next = temp;
+
+		if(p && p != c)
+			p->next = c;
+		if(pc && pc != sel)
+			pc->next = sel;
+
+		if(sel == clients)
+			clients = c;
+		else if(c == clients)
+			clients = sel;
+
+		arrange();
+	}
+}
+
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {    
     MSG msg;
