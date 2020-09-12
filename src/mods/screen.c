59bb3295640d776e328ba20ef222b5d602270d16
Prabir Shrestha <mail@prabir.me>
2020-09-12 14:18:45 -0700

    open dwmscreenmod

--- src/mods/screen.c
+++ src/mods/screen.c
@@ -1,5 +1,14 @@
 #include "screen.h"
 
+#include <lauxlib.h>
+#include <lua.h>
+#include <lualib.h>
+
+static const struct  luaL_reg dwmscreenmod[] = {
+	{ NULL, NULL }
+};
+
 int dwmmod_openscreen(lua_State *L) {
+	luaL_register(L, "dwm.screen", dwmscreenmod);
 	return 1;
 }
