a4edd0706f9b1f5e70963f906614e18baad8567c
Prabir Shrestha <mail@prabir.me>
2020-09-12 19:47:59 -0700

    add getClient(id)

--- src/mods/client.c
+++ src/mods/client.c
@@ -7,6 +7,7 @@
 #include <lualib.h>
 
 #include <windows.h>
+#include <dwmapi.h>
 
 typedef struct EnumWindowsState {
 	lua_State *L;
@@ -15,10 +16,16 @@ typedef struct EnumWindowsState {
 
 BOOL CALLBACK EnumWindowsCallback(HWND hwnd, LPARAM lParam);
 
+static const char *getclienttitle(HWND hwnd);
+static const char *getclientclassname(HWND hwnd);
+static BOOL iscloaked(HWND hwnd);
+
 static int modclient_getClients(lua_State *L);
+static int modclient_getClient(lua_State *L);
 
 static const struct  luaL_reg dwmclientmod[] = {
 	{ "getClients", modclient_getClients },
+	{ "getClient", modclient_getClient },
 	{ NULL, NULL }
 };
 
@@ -49,4 +56,79 @@ BOOL CALLBACK EnumWindowsCallback(HWND hwnd, LPARAM lParam) {
 	lua_settable(state->L, -3);
 
 	return TRUE;
+}
+
+static int modclient_getClient(lua_State *L) {
+	uint32_t id = (uint32_t)luaL_checknumber(L, 1); /* first arg */
+	HWND hwnd = (HWND)id;
+
+	if (!IsWindow(hwnd)) {
+		lua_pushnil(L);
+		return 1;
+	}
+
+	lua_newtable(L);
+
+	lua_pushstring(L, "id");
+	lua_pushnumber(L, id);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "visible");
+	lua_pushboolean(L, IsWindowVisible(hwnd) ? 1 : 0);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "title");
+	lua_pushstring(L, getclienttitle(hwnd));
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "classname");
+	lua_pushstring(L, getclientclassname(hwnd));
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "parent");
+	HWND parent = GetParent(hwnd);
+	if (parent)
+		lua_pushnumber(L, (uint32_t)parent);
+	else
+		lua_pushnil(L);
+	lua_settable(L, -3);
+
+	lua_pushstring(L, "cloaked");
+	lua_pushboolean(L, iscloaked(hwnd) ? 1 : 0);
+	lua_settable(L, -3);
+
+	return 1;
+}
+
+const char
+*getclienttitle(HWND hwnd) {
+    static wchar_t buf[500];
+    GetWindowTextW(hwnd, buf, sizeof buf);
+
+	char str[500];
+	wcstombs(str, buf, sizeof(str));
+
+    return str;
+}
+
+const char
+*getclientclassname(HWND hwnd) {
+    static wchar_t buf[500];
+    GetClassNameW(hwnd, buf, sizeof buf);
+
+	char str[500];
+	wcstombs(str, buf, sizeof(str));
+
+    return str;
+}
+
+BOOL
+iscloaked(HWND hwnd) {
+    int cloaked_val;
+    HRESULT h_res = DwmGetWindowAttribute(hwnd, DWMWA_CLOAKED, &cloaked_val, sizeof(cloaked_val));
+
+    if (h_res != S_OK)
+        cloaked_val = 0;
+
+    return cloaked_val ? TRUE : FALSE;
 }
