ed20f43084028a545425c894a4f0c47afd6deb4c
Prabir Shrestha <mail@prabir.me>
2020-09-27 15:35:23 -0700

    add dwm_setstate and dwm_getstate

--- src/mods/dwm.h
+++ src/mods/dwm.h
@@ -5,6 +5,15 @@
 #include <lua.h>
 #include <lualib.h>
 
+#include <windows.h>
+
+typedef struct {
+    HINSTANCE hInstance;
+} DwmState;
+
 int luaopen_dwm(lua_State *L);
+
+void dwm_setstate(lua_State *L, DwmState *state);
+DwmState *dwm_getstate(lua_State *L);
 
 #endif
