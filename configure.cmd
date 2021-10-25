1e3faaae3c4f51a0742796ad7df8a2b2b9f85a95
Prabir Shrestha <mail@prabir.me>
2021-10-25 15:12:26 -0700

    build release by default

--- configure.cmd
+++ configure.cmd
@@ -1,2 +1,2 @@
-cmake -S . -B build -G "Unix Makefiles" -D USE_LUAJIT=ON || exit /b
+cmake -S . -B build -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D USE_LUAJIT=ON || exit /b
 copy build\compile_commands.json .\
