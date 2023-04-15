0fc60a590799c0576e8b2fd63169b56643853738
Prabir Shrestha <mail@prabir.me>
2023-04-14 18:30:22 -0700

    bump versio to v0.1.1

--- build.cmd
+++ build.cmd
@@ -6,6 +6,6 @@ zig cc ^
     extern/lua/src/lapi.c extern/lua/src/lauxlib.c extern/lua/src/lbaselib.c extern/lua/src/lcode.c extern/lua/src/ldblib.c extern/lua/src/ldebug.c extern/lua/src/ldo.c extern/lua/src/ldump.c extern/lua/src/lfunc.c extern/lua/src/lgc.c extern/lua/src/linit.c extern/lua/src/liolib.c extern/lua/src/llex.c extern/lua/src/lmathlib.c extern/lua/src/lmem.c extern/lua/src/loadlib.c extern/lua/src/lobject.c extern/lua/src/lopcodes.c extern/lua/src/loslib.c extern/lua/src/lparser.c extern/lua/src/lstate.c extern/lua/src/lstring.c extern/lua/src/lstrlib.c extern/lua/src/ltable.c extern/lua/src/ltablib.c extern/lua/src/ltm.c extern/lua/src/lundump.c extern/lua/src/lvm.c extern/lua/src/lzio.c ^
     -Iextern/lua-compat-5.3 ^
     extern/lua-compat-5.3/compat-5.3.c ^
-    -DPROJECT_NAME='"dwm-win32"' -DPROJECT_VER='"i0.1.0"' -DPROJECT_VER_MAJOR=0 -DPROJECT_VER_MINOR=1 -DPROJECT_VER_PATCH=0 ^
+    -DPROJECT_NAME='"dwm-win32"' -DPROJECT_VER='"0.1.1"' -DPROJECT_VER_MAJOR=0 -DPROJECT_VER_MINOR=1 -DPROJECT_VER_PATCH=1 ^
     -DNDEBUG -O2 -s ^
     -target x86_64-windows-msvc -std=c99 -pedantic -Wall
